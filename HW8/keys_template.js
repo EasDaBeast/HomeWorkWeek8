06298a72acfcdaa29fc01758af60b02337e17394
EasDaBeast <33886412+EasDaBeast@users.noreply.github.com>
2018-06-20 20:21:20 -0400

    Add files via upload

---
+++ HW8/keys_template.js
@@ -0,0 +1,15 @@
+exports.twitter = {
+    "consumer_key"       : "vkAcYzGqvK9zT6AV6ZFqQyurS",
+    "consumer_secret"    : "mzXl6aTn2RmAIdbkiAkcruRbIXEcHQOqDRCqH50k3whZBIHCN8",
+    "access_token_key"   : "379718416-OpKQXBoPwNS3c82xqoPzFKeGh3xjYhr1u9Yb4ztM",
+    "access_token_secret": "tqwB5APjADfA4k0aUllR45dpB7AqD4WrZizX9ZiSy4rdN",
+}
+
+exports.spotify = {
+    "id"    : "54b55af5236d4619bb85a5309a0d1ade",
+    "secret": "47a74a9e3edc4faf80a85e5236a14f3d"
+}
+
+exports.omdb = {
+    "key": "8eba232f"
+}
